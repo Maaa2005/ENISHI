bd4cdfc0c718095b421d9afbb3e03dff59b4891e
Masashi <212696738+Maaa2005@users.noreply.github.com>
2026-07-19 23:15:33 +0900

    ci: add repository quality gates

--- services/local-core/alembic_migrations/env.py
+++ services/local-core/alembic_migrations/env.py
@@ -3,8 +3,8 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 from enishi_core.models import Base
+from sqlalchemy import engine_from_config, pool
 
 config = context.config
 
